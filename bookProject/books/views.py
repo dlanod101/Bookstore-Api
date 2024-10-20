fe2d069199ea0bf6bf9355aca2b7413e204486c8
Donald <dniskhe@gmail.com>
2024-10-21 00:23:53 +0100

    Code Commit

--- bookProject/books/views.py
+++ bookProject/books/views.py
@@ -4,23 +4,48 @@ from .serializers import BookSerializer
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 # Create your views here.
 
 class BookCreate(generics.CreateAPIView):
+    """
+    -`POST` Creates a new Book
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    """
+    -`GET` Returns a particular existing Book by its id
+    -`PUT` Updates an existing Book
+    -`PATCH` Partially updates an existing Book
+    -`DELETE` Deletes an existing Book
+    """
+    
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = 'pk'
 
 class BookFind(APIView):
+    """
+    -`GET` Returns a Books with specific title but returns all books if no book matches said title
+    """
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'title',
+                openapi.IN_QUERY,
+                description="Returns a Books with specific title but returns all books if no book matches said title",
+                type=openapi.TYPE_STRING,
+            ),
+        ],
+    )
     def get(self, request, format=None):
         title = request.query_params.get("title", "")
 
         if title:
-            book = Book.objects.filter(title_icontains=title)
+            book = Book.objects.filter(title__icontains=title)
 
         else:
             book = Book.objects.all()

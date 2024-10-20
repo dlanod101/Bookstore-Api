3587168f6bee36d93a1ab7105dd8fce36c16c3d8
Donald <dniskhe@gmail.com>
2024-10-20 15:34:17 +0100

    Book Model Commit

--- bookProject/books/models.py
+++ bookProject/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    available = models.BooleanField(default=False)
+    date_published = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
